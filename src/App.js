b185a286f88b95cbc43de65637e49033ff437186
Marián Čekan <majkyy@MacBook-Pro-uzivatela-Marian-2.local>
2023-09-18 13:25:03 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// Now you can use wasm as an object that contains your exported functions and memory.
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            wasm: {}, // Initialize wasm as an empty object
+        };
+    }
+
+    componentDidMount() {
+        this.loadWasm();
+    }
+
+    loadWasm = async () => {
+        try {
+            const wasm = await import('external');
+            this.setState({ wasm });
+        } catch (err) {
+            console.error(`Unexpected error in loadWasm. [Message: ${err.message}]`);
+        }
+    };
+
+    render() {
+        const { wasm } = this.state;
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>Edit <code>src/App.js</code> and save to reload.</p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                    <div>
+                        <div>Name: <input type='text' onChange={(e) => this.setState({ name: e.target.value })} /></div>
+                        <div>{wasm.hello && wasm.hello(this.state.name)}</div>
+                    </div>
+                    <div>
+                        <div>
+                            Add:
+                            <input type='text' onChange={(e) => this.setState({ addNum1: e.target.value })} />
+                            <input type='text' onChange={(e) => this.setState({ addNum2: e.target.value })} />
+                        </div>
+                        <div>
+                            Result: {wasm.add && wasm.add(this.state.addNum1 || 0, this.state.addNum2 || 0)}
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            Sub:
+                            <input type='text' onChange={(e) => this.setState({ subNum1: e.target.value })} />
+                            <input type='text' onChange={(e) => this.setState({ subNum2: e.target.value })} />
+                        </div>
+                        <div>
+                            Result: {wasm.sub && wasm.sub(this.state.subNum1 || 0, this.state.subNum2 || 0)}
+                        </div>
+                    </div>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
